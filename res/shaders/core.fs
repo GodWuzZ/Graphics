b7130ad0e68ddf6546bd184b16f758200b013a5e
GodWuzZ <shermanfdy@gmail.com>
2021-09-30 10:52:16 +0800

    实现了渐变色和不同颜色交替出现

--- res/shaders/core.fs
+++ res/shaders/core.fs
@@ -3,9 +3,11 @@ out vec4 color;
 in vec3 vertexColor;
 in vec2 ourTextCoord;
 uniform float time;
+uniform vec4 ourColor;
 uniform sampler2D texture0;
 void main()
 {
+	color = ourColor;
 	// color = vec4(vertexColor,1.0f);
-	color = texture(texture0,ourTextCoord);
+	// color = texture(texture0,ourTextCoord);
 };
